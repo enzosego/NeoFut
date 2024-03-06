28de8a5632b031cca0b9038fa148863a1d02d7ad
Enzo <Enzo_seg@outlook.com>
2024-03-06 12:04:34 -0300

    fix: TopAssistsTab shows incorrect layout when getting and empty list

--- app/src/main/java/com/ensegov/neofut/competition_detail/presentation/player_stats/assists/TopAssistsTable.kt
+++ app/src/main/java/com/ensegov/neofut/competition_detail/presentation/player_stats/assists/TopAssistsTable.kt
@@ -7,8 +7,10 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import com.ensegov.neofut.R
 import com.ensegov.neofut.competition_detail.presentation.player_stats.common.StatsColumnNames
 import com.ensegov.neofut.competition_detail.presentation.player_stats.common.StatsTitle
@@ -21,31 +23,40 @@ fun TopAssistsTable(
     playerStats: () -> List<PlayerStatsUiData>,
     modifier: Modifier = Modifier
 ) {
-    LazyColumn(
-        modifier = modifier.fillMaxWidth()
-    ) {
-        item {
-            StatsTitle(text = stringResource(R.string.top_assists))
-        }
-        item {
-            StatsColumnNames {
-                Text(
-                    text = stringResource(R.string.player_name),
-                    textAlign = TextAlign.Center,
-                    modifier = Modifier.weight(.85f)
-                )
-                Text(
-                    text = stringResource(R.string.assists),
-                    textAlign = TextAlign.Center,
-                    modifier = Modifier.weight(.15f)
+    when(playerStats().size) {
+        0 -> Text(
+            text = stringResource(R.string.no_assists_list),
+            fontWeight = FontWeight.Bold,
+            textAlign = TextAlign.Center,
+            fontSize = 20.sp,
+            modifier = modifier.fillMaxWidth()
+        )
+        else -> LazyColumn(
+            modifier = modifier.fillMaxWidth()
+        ) {
+            item {
+                StatsTitle(text = stringResource(R.string.top_assists))
+            }
+            item {
+                StatsColumnNames {
+                    Text(
+                        text = stringResource(R.string.player_name),
+                        textAlign = TextAlign.Center,
+                        modifier = Modifier.weight(.85f)
+                    )
+                    Text(
+                        text = stringResource(R.string.assists),
+                        textAlign = TextAlign.Center,
+                        modifier = Modifier.weight(.15f)
+                    )
+                }
+            }
+            items(playerStats()) { stats ->
+                PlayerAssistsRow(
+                    playerStats = stats,
+                    modifier = Modifier.fillMaxWidth()
                 )
             }
-        }
-        items(playerStats()) { stats ->
-            PlayerAssistsRow(
-                playerStats = stats,
-                modifier = Modifier.fillMaxWidth()
-            )
         }
     }
 }
